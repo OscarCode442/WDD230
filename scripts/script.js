f213ebdf1fddef16d39ad0e235d9f35534a26b58
OscarCode442 <ukaegbueze442@yahoo.com>
2023-01-10 20:30:15 +0100

    fix last modified bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const options = {year: 'numeric'}
 document.getElementById('year').textContent = new Date().toLocaleDateString('en-us', options)
 
-const option2 = {month: 'numeric', day: 'numeric', year: 'numeric'}
+// const option2 = {month: 'numeric', day: 'numeric', year: 'numeric'}
 // document.getElementById('date').textContent = new Date().toLocaleDateString('en-us', option2)
 
 // const option1 = new Date()
